6d11a95800c97ae674d41cc9b156bb1c72a3bef2
manuLlanos <26463463+manuLlanos@users.noreply.github.com>
2024-09-28 16:51:41 -0300

    Agregar function getPrimeFactors

--- script.js
+++ script.js
@@ -2,14 +2,42 @@
 
 function isPrime(num) {
     if (num <= 1) return false;
-    if (num === 2) return true;
-    if (num % 2 === 0) return false;
+    if (num == 2) return true;
+    if (num % 2 == 0) return false;
 
     const LIMIT = Math.floor(Math.sqrt(num));
 
     for (let i = 3; i < LIMIT; i++) {
-        if (num % i === 0) return false;
+        if (num % i == 0) return false;
     }
 
     return true;
+}
+
+function getPrimeFactors(num) {
+    if (num <= 0) return null;
+    if (num == 1) return 1;
+    if (isPrime(num)) return num;
+
+    let factors = [];
+    let value = num;
+
+
+    while (!isPrime(value)) {
+        for (let i = 2; i <= value; i++) {
+            if (value % i == 0) {
+                factors.push(i);
+                value /= i;
+                break;
+            }
+        }
+    }
+
+    factors.push(value);
+
+    return factors.sort((a, b) => a - b);
+}
+
+for (let i = 1; i < 101; i++) {
+    console.log(`${i} -> ${getPrimeFactors(i)}`);
 }
